cdac7974f685d82dfeae6a071de502a8a0198750
emrahsenturk <emrahsenturkk@gmail.com>
2023-12-17 14:30:20 +0300

    NewGame and Game pages added.

--- OnlineScrumPoker/Hubs/ScrumPokerHub.cs
+++ OnlineScrumPoker/Hubs/ScrumPokerHub.cs
@@ -7,20 +7,25 @@ public class ScrumPokerHub : Hub
 {
     public override Task OnConnectedAsync()
     {
-        Clients.All.SendAsync("ReceiveMessage", "system", $"{Context.ConnectionId} joined the online scrum poker.");
+        //Clients.All.SendAsync("ReceiveMessage", "system", $"{Context.ConnectionId} joined the online scrum poker.");
 
         return base.OnConnectedAsync();
     }
 
     public override Task OnDisconnectedAsync(Exception? exception)
     {
-        Clients.All.SendAsync("ReceiveMessage", "system", $"{Context.ConnectionId} left the online scrum poker.");
+        //Clients.All.SendAsync("ReceiveMessage", "system", $"{Context.ConnectionId} left the online scrum poker.");
 
         return base.OnDisconnectedAsync(exception);
     }
 
-    public void SendMessage(string name, string message)
+    public void StartNewGame()
     {
-        Clients.All.SendAsync("ReceiveMessage", name, message);
+        Clients.All.SendAsync("NavigateToGame", Guid.NewGuid());
+    }
+
+    public void Vote(string gameId, int vote)
+    {
+        Clients.All.SendAsync("UpdateVotes", gameId, vote);
     }
 }
